cd0f956e90bcce2ee7da7000eba0252f4ac7e231
Godfred Kwesi Agyeman <godfredjohn5@gmail.com>
2023-11-26 00:36:13 +0000

    Created a UI for the game

--- script.js
+++ script.js
@@ -1,84 +1,105 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const rockButton = document.querySelector('.btnRock');
+    const paperButton = document.querySelector('.btnPaper');
+    const scissorButton = document.querySelector('.btnScissor');
+    const userScoreElement = document.getElementById('userScore');
+    const computerScoreElement = document.getElementById('computerScore');
+    const roundResultsElement = document.getElementById('roundResults');
 
-function ComputerChoice(){
-    const choice_num= Math.floor(Math.random() * 3) + 1;
-    let choice;
-    switch(choice_num) {
-        case 1:
-            choice = "ROCK";
-            break;
-        case 2: 
-            choice = "PAPER";
-            break;
-        case 3:
-            choice = "SCISSORS";
-            break;
-    }
-    return choice;
-}
-
-const computerChoice = ComputerChoice();
-var userChoice = "ROCK";
-
-
-
-function rounds(computerChoice, userChoice) {
-    let results;
-    if (computerChoice.toUpperCase() === userChoice.toUpperCase()) {
-        results = "DRAW";
-    } else if (computerChoice.toUpperCase() === "PAPER" && userChoice.toUpperCase() === "ROCK") {
-        results = "I WIN";
-    } else if (computerChoice.toUpperCase() === "ROCK" && userChoice.toUpperCase() === "PAPER") {
-        results = "YOU WIN";
-    }else if (computerChoice.toUpperCase() === "SCISSORS" && userChoice.toUpperCase() === "ROCK") {
-        results = "YOU WIN";
-    } else if (computerChoice.toUpperCase() === "ROCK" && userChoice.toUpperCase() === "SCISSORS") {
-        results = "I WIN";
-    }else if (computerChoice.toUpperCase() === "PAPER" && userChoice.toUpperCase() === "SCISSORS") {
-        results = "YOU WIN";
-    } else if (computerChoice.toUpperCase() === "SCISSORS" && userChoice.toUpperCase() === "PAPER") {
-        results = "I WIN";
-    }
-
-    return results;
-}
-
-
-//Game function
-
-function game() {
-    
-    let computerScore = 0;
     let userScore = 0;
+    let computerScore = 0;
+    let gamesPlayed = 0;
+    
+    //ComputerChoice
+    function ComputerChoice() {
+        const choice_num = Math.floor(Math.random() * 3) + 1;
+        let choice;
+        switch (choice_num) {
+            case 1:
+                choice = "ROCK";
+                break;
+            case 2:
+                choice = "PAPER";
+                break;
+            case 3:
+                choice = "SCISSORS";
+                break;
+        }
+        return choice;
+    }
 
-    for (let round = 1; round <= 5; round++) {
-     
-        const computerChoice = ComputerChoice();
-        let userChoice;
-        do {
-            userChoice = prompt("Choose ROCK, PAPER, or SCISSORS").toUpperCase();
-        } while (!["ROCK", "PAPER", "SCISSORS"].includes(userChoice));
-
-        const result = rounds(computerChoice, userChoice);
-
-     
-        if (result === "I WIN") {
+    function rounds(computerChoice, userChoice) {
+        let results;
+        if (computerChoice.toUpperCase() === userChoice.toUpperCase()) {
+            results = "DRAW";
+        } else if (computerChoice.toUpperCase() === "PAPER" && userChoice.toUpperCase() === "ROCK") {
+            results = "I WIN";
             computerScore++;
-        } else if (result === "YOU WIN") {
+        } else if (computerChoice.toUpperCase() === "ROCK" && userChoice.toUpperCase() === "PAPER") {
+            results = "YOU WIN";
             userScore++;
+        } else if (computerChoice.toUpperCase() === "SCISSORS" && userChoice.toUpperCase() === "ROCK") {
+            results = "YOU WIN";
+            userScore++;
+        } else if (computerChoice.toUpperCase() === "ROCK" && userChoice.toUpperCase() === "SCISSORS") {
+            results = "I WIN";
+            computerScore++;
+        } else if (computerChoice.toUpperCase() === "PAPER" && userChoice.toUpperCase() === "SCISSORS") {
+            results = "YOU WIN";
+            userScore++;
+        } else if (computerChoice.toUpperCase() === "SCISSORS" && userChoice.toUpperCase() === "PAPER") {
+            results = "I WIN";
+            computerScore++;
         }
 
-        console.log(`Round ${round}: Computer chose ${computerChoice}, User chose ${userChoice}, Result: ${result}`);
+        // Display the results after each game
+        roundResultsElement.textContent = `Game ${gamesPlayed + 1}: Computer chose ${computerChoice}, User chose ${userChoice}, Result: ${results}`;
+
+        
+        userScoreElement.textContent = userScore;
+        computerScoreElement.textContent = computerScore;
+
+        
+        if (++gamesPlayed === 5) {
+            announceWinner();
+            //userScore =0;
+            //computerScore =0;
+
+            
+        }
+        
     }
 
-   
-    if (computerScore > userScore) {
-        console.log("Computer wins the game!");
-    } else if (userScore > computerScore) {
-        console.log("User wins the game!");
-    } else {
-        console.log("It's a draw!");
+    function announceWinner() {
+        if (userScore > computerScore) {
+            roundResultsElement.textContent = `User wins the round! User: ${userScore}, Computer: ${computerScore}`;
+        } else if (computerScore > userScore) {
+            roundResultsElement.textContent = `Computer wins the round! User: ${userScore}, Computer: ${computerScore}`;
+        } else {
+            roundResultsElement.textContent = `It's a draw! User: ${userScore}, Computer: ${computerScore}`;
+        }
+
+        
+         userScore = 0;
+         computerScore = 0;
+         gamesPlayed =0;
+        
     }
-}
 
+    function playGame(userChoice) {
+        const computerChoice = ComputerChoice();
+        rounds(computerChoice, userChoice);
+    }
 
-game();
+    rockButton.addEventListener('click', function() {
+        playGame("ROCK");
+    });
+
+    paperButton.addEventListener('click', function() {
+        playGame("PAPER");
+    });
+
+    scissorButton.addEventListener('click', function() {
+        playGame("SCISSORS");
+    });
+});
